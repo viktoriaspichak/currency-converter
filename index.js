02e3a878c8b539c4a7466327e74ce1d720acba43
viktoriaspichak <vspichak@ukr.net>
2023-03-09 20:01:32 +0200

    finished task

---
+++ index.js
@@ -0,0 +1,81 @@
+$( document ).ready(function() {
+    getCurrencyRates();
+    $('#input1').on('input', calculateForInput2);
+    $('#select1').on('change',calculateForInput2);
+    $('#input2').on('input', calculateForInput1);
+    $('#select2').on('change',calculateForInput1);
+
+});
+
+let usdRate;
+let eurRate;
+
+function getCurrencyRates(){
+    $.get("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json", function( data ) {
+        usdRate = data.find(x => x.cc === 'USD').rate;
+        eurRate = data.find(x => x.cc === 'EUR').rate;
+
+        $('#usd').text(usdRate)
+        $('#eur').text(eurRate)
+    });
+}
+
+function calculateForInput2() {
+    let convertFrom =  $('#select1').val();
+    let convertTo =  $('#select2').val();
+    let amount = $('#input1').val();
+    let converted = convert(convertFrom, convertTo, amount)
+    $('#input2').val(converted);
+}
+
+function calculateForInput1() {
+    let convertFrom =  $('#select2').val();
+    let convertTo =  $('#select1').val();
+    let amount = $('#input2').val();
+    let converted = convert(convertFrom, convertTo, amount)
+    $('#input1').val(converted);
+}
+
+
+function convert(from, to, amount){
+if (from == to){
+    return amount;
+}
+
+if(from == 'usd'){
+    let convertedToUah = amount * usdRate;
+
+    if(to == 'uah') {
+        return convertedToUah;
+    }
+
+    if(to == 'eur')
+    {
+        return convertedToUah/eurRate;
+    }
+
+}
+
+if(from == 'eur'){
+        let convertedToUah = amount * eurRate;
+
+        if(to == 'uah') {
+            return convertedToUah;
+        }
+
+        if(to == 'usd')
+        {
+            return convertedToUah/usdRate;
+        }
+
+    }
+
+if (from == 'uah') {
+ if(to == 'usd'){
+     return amount/ usdRate
+ }
+ if(from == 'eur'){
+     return amount/eurRate;
+ }
+}
+}
